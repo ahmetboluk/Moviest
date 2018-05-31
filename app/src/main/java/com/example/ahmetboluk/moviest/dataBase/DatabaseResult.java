9397a0bcb4bf077317f078cfa5cd8d5f7d920878
ahmetboluk <ahmetboluk@outlook.com.tr>
2018-05-31 14:06:41 +0300

    2.0

---
+++ app/src/main/java/com/example/ahmetboluk/moviest/dataBase/DatabaseResult.java
@@ -0,0 +1,15 @@
+package com.example.ahmetboluk.moviest.dataBase;
+
+import android.provider.BaseColumns;
+
+public class DatabaseResult {
+    public int id;
+    public int itemid;
+    public String name;
+    public String rate;
+    public String thumbnailUrl;
+    public int type ;
+    public String year ;
+    public int categoryId ;
+
+}
--- app/src/main/java/com/example/ahmetboluk/moviest/myFragment/DialogFragment.java
+++ app/src/main/java/com/example/ahmetboluk/moviest/myFragment/DialogFragment.java
@@ -14,7 +14,10 @@ import android.widget.Toast;
 
 import com.example.ahmetboluk.moviest.MainActivity;
 import com.example.ahmetboluk.moviest.R;
+import com.example.ahmetboluk.moviest.dataBase.DatabaseResult;
 import com.example.ahmetboluk.moviest.dataBase.MoviestContract;
+
+import java.util.ArrayList;
 
 public class DialogFragment extends android.support.v4.app.DialogFragment {
 
@@ -23,7 +26,8 @@ public class DialogFragment extends android.support.v4.app.DialogFragment {
     }
 
     Button addToWatch, addWatched, addFavorites;
-
+    DatabaseResult databaseResult;
+    Cursor cursor;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -39,29 +43,74 @@ public class DialogFragment extends android.support.v4.app.DialogFragment {
         addToWatch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ContentValues contentValues = new ContentValues();
-                contentValues.put(MoviestContract.MoviestItemEntry.COLUMN_ID, String.valueOf(getArguments().getInt("movie_id")));
-                contentValues.put(MoviestContract.MoviestItemEntry.COLUMN_NAME, getArguments().getString("name"));
-                contentValues.put(MoviestContract.MoviestItemEntry.COLUMN_YEAR, getArguments().getString("date"));
-                contentValues.put(MoviestContract.MoviestItemEntry.COLUMN_THUMBNAIL_URL, getArguments().getString("poster"));
-                contentValues.put(MoviestContract.MoviestItemEntry.COLUMN_RATE, getArguments().getString("rate"));
-                contentValues.put(MoviestContract.MoviestItemEntry.COLUMN_TYPE, getArguments().getInt("selected"));
-                contentValues.put(MoviestContract.MoviestItemEntry.COLUMN_CATEGORY, 0);
-                Uri _uri = MainActivity.getAppContext().getContentResolver().insert(MoviestContract.MoviestItemEntry.CONTENT_URI, contentValues);
-                Toast.makeText(getContext(), "OLDUMU" + _uri, Toast.LENGTH_LONG).show();
-
+                if (databaseResult.itemid == getArguments().getInt("movie_id") && databaseResult.categoryId == 2) {
+                    Toast.makeText(getContext(), "First Remove From  Watched", Toast.LENGTH_SHORT).show();
+                } else if (databaseResult.itemid == getArguments().getInt("movie_id") && databaseResult.categoryId == 3) {
+                    Toast.makeText(getContext(), "Firstly Remove From Favorites ", Toast.LENGTH_SHORT).show();
+                } else if (!(databaseResult.itemid == getArguments().getInt("movie_id") && databaseResult.categoryId == 1)) {
+                    ContentValues contentValues = new ContentValues();
+                    contentValues.put(MoviestContract.MoviestItemEntry.COLUMN_ID, String.valueOf(getArguments().getInt("movie_id")));
+                    contentValues.put(MoviestContract.MoviestItemEntry.COLUMN_NAME, getArguments().getString("name"));
+                    contentValues.put(MoviestContract.MoviestItemEntry.COLUMN_YEAR, getArguments().getString("date"));
+                    contentValues.put(MoviestContract.MoviestItemEntry.COLUMN_THUMBNAIL_URL, getArguments().getString("poster"));
+                    contentValues.put(MoviestContract.MoviestItemEntry.COLUMN_RATE, getArguments().getString("rate"));
+                    contentValues.put(MoviestContract.MoviestItemEntry.COLUMN_TYPE, getArguments().getInt("selected"));
+                    contentValues.put(MoviestContract.MoviestItemEntry.COLUMN_CATEGORY, 1);
+                    Uri _uri = MainActivity.getAppContext().getContentResolver().insert(MoviestContract.MoviestItemEntry.CONTENT_URI, contentValues);
+                    getDb();
+                    dismiss();
+                } else {
+                    dismiss();
+                    Toast.makeText(getContext(), "TO BE CONTINUED", Toast.LENGTH_SHORT).show();
+                }
             }
         });
         addWatched.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                if (databaseResult.itemid == getArguments().getInt("movie_id") && databaseResult.categoryId == 1) {
+                    Toast.makeText(getContext(), "First Remove From Watch", Toast.LENGTH_SHORT).show();
+                } else if (databaseResult.itemid == getArguments().getInt("movie_id") && databaseResult.categoryId == 3) {
+                    Toast.makeText(getContext(), "Firstly Remove From Favorites ", Toast.LENGTH_SHORT).show();
+                } else if (!(databaseResult.itemid == getArguments().getInt("movie_id") && databaseResult.categoryId == 2)) {
+                    ContentValues contentValues = new ContentValues();
+                    contentValues.put(MoviestContract.MoviestItemEntry.COLUMN_ID, String.valueOf(getArguments().getInt("movie_id")));
+                    contentValues.put(MoviestContract.MoviestItemEntry.COLUMN_NAME, getArguments().getString("name"));
+                    contentValues.put(MoviestContract.MoviestItemEntry.COLUMN_YEAR, getArguments().getString("date"));
+                    contentValues.put(MoviestContract.MoviestItemEntry.COLUMN_THUMBNAIL_URL, getArguments().getString("poster"));
+                    contentValues.put(MoviestContract.MoviestItemEntry.COLUMN_RATE, getArguments().getString("rate"));
+                    contentValues.put(MoviestContract.MoviestItemEntry.COLUMN_TYPE, getArguments().getInt("selected"));
+                    contentValues.put(MoviestContract.MoviestItemEntry.COLUMN_CATEGORY, 2);
+                    Uri _uri = MainActivity.getAppContext().getContentResolver().insert(MoviestContract.MoviestItemEntry.CONTENT_URI, contentValues);
+                    getDb();
+                } else {
+                    dismiss();
+                    Toast.makeText(getContext(), "TO BE CONTINUED", Toast.LENGTH_SHORT).show();
+                }
             }
         });
         addFavorites.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                if (databaseResult.itemid == getArguments().getInt("movie_id") && databaseResult.categoryId == 1) {
+                    Toast.makeText(getContext(), "First Remove From Watch", Toast.LENGTH_SHORT).show();
+                } else if (databaseResult.itemid == getArguments().getInt("movie_id") && databaseResult.categoryId == 2) {
+                    Toast.makeText(getContext(), "Firstly Remove From Watched ", Toast.LENGTH_SHORT).show();
+                } else if (!(databaseResult.itemid == getArguments().getInt("movie_id") && databaseResult.categoryId == 3)) {
+                    ContentValues contentValues = new ContentValues();
+                    contentValues.put(MoviestContract.MoviestItemEntry.COLUMN_ID, String.valueOf(getArguments().getInt("movie_id")));
+                    contentValues.put(MoviestContract.MoviestItemEntry.COLUMN_NAME, getArguments().getString("name"));
+                    contentValues.put(MoviestContract.MoviestItemEntry.COLUMN_YEAR, getArguments().getString("date"));
+                    contentValues.put(MoviestContract.MoviestItemEntry.COLUMN_THUMBNAIL_URL, getArguments().getString("poster"));
+                    contentValues.put(MoviestContract.MoviestItemEntry.COLUMN_RATE, getArguments().getString("rate"));
+                    contentValues.put(MoviestContract.MoviestItemEntry.COLUMN_TYPE, getArguments().getInt("selected"));
+                    contentValues.put(MoviestContract.MoviestItemEntry.COLUMN_CATEGORY, 3);
+                    Uri _uri = MainActivity.getAppContext().getContentResolver().insert(MoviestContract.MoviestItemEntry.CONTENT_URI, contentValues);
+                    getDb();
+                } else {
+                    dismiss();
+                    Toast.makeText(getContext(), "TO BE CONTINUED", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
@@ -79,22 +128,47 @@ public class DialogFragment extends android.support.v4.app.DialogFragment {
                 "moviestItem.rate",
                 "moviestItem.type",
                 "moviestItem.categoryId"};
-        Cursor cursor = MainActivity.getAppContext().getContentResolver().query(MoviestContract.MoviestItemEntry.CONTENT_URI, projection, "itemid=?", new String[]{String.valueOf(getArguments().getInt("movie_id"))}, null, null);
-        String kayitlar = "";
-        if (cursor!=null) {
+        cursor = MainActivity.getAppContext().getContentResolver().query(MoviestContract.MoviestItemEntry.CONTENT_URI, projection, "itemid=?", new String[]{String.valueOf(getArguments().getInt("movie_id"))}, null, null);
+        databaseResult = new DatabaseResult();
+        if (cursor != null) {
             while (cursor.moveToNext()) {
-                for (int j = 0; j <= 6; j++) {
-                    kayitlar += cursor.getString(j) + " - ";
-                }
-                kayitlar += "\n";
+                databaseResult.id = cursor.getInt(0);
+                databaseResult.itemid = cursor.getInt(1);
+                databaseResult.name = cursor.getString(2);
+                databaseResult.year = cursor.getString(3);
+                databaseResult.thumbnailUrl = cursor.getString(4);
+                databaseResult.rate = cursor.getString(5);
+                databaseResult.type = cursor.getInt(6);
+                databaseResult.categoryId = cursor.getInt(7);
             }
             cursor.close();
         }
-        Log.i("TAG", "onClick: " + kayitlar);
-        Toast.makeText(getContext(), "OLDUMU" + kayitlar, Toast.LENGTH_LONG).show();
-
-
-
+        switch (databaseResult.categoryId) {
+            case 1:
+                if (databaseResult.itemid == getArguments().getInt("movie_id")) {
+                    addToWatch.setText("Delete Watch");
+                    addWatched.setText("Move to Watched");
+                    addFavorites.setText("Move to Favorities");
+                }
+                break;
+            case 2:
+                if (databaseResult.itemid == getArguments().getInt("movie_id")) {
+                    addWatched.setText("Delete Watched");
+                    addFavorites.setText("Move to Favorities");
+                    addToWatch.setText("Move to Watch");
+                }
+                break;
+            case 3:
+                if (databaseResult.itemid == getArguments().getInt("movie_id")) {
+                    addFavorites.setText("Delete Favorities");
+                    addWatched.setText("Move to Watched");
+                    addToWatch.setText("Move to Watch");
+                }
+                break;
+            default:
+                addToWatch.setText("Add To Watch");
+                addWatched.setText("Add Watched");
+                addFavorites.setText("Add Favorities");
+        }
     }
-
 }
